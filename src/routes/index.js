42b1274d69240b3f36b284264bd1136f87f45659
zzzhim <3218566389@qq.com>
2020-01-26 22:41:46 +0800

    ejs初使用

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 /*
  * Author: your name
  * Date: 2020-01-26 19:05:34
- * LastEditTime: 2020-01-26 21:20:54
+ * LastEditTime: 2020-01-26 22:32:16
  * LastEditors: Please set LastEditors
  * Description: In User Settings Edit
  * FilePath: \koa-weibo\src\routes\index.js
@@ -9,19 +9,46 @@
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
+    await ctx.render('index', {
+        title: 'Hello Koa 2!'
+    })
 })
 
 router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
+    ctx.body = 'koa2 string'
 })
 
 router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
-  }
+    ctx.body = {
+        title: 'koa2 json'
+    }
+})
+
+
+/**
+ *
+ * 动态参数演示
+ * @returns ejsDemo
+ */
+router.get('/ejsDemo', async (ctx, next) => {
+    await ctx.render('ejsDemo', {
+        title: 'Hello Koa 2!',
+        isMe: false,
+        list: [
+            {
+                id: 1,
+                title: "a"
+            },
+            {
+                id: 2,
+                title: "b"
+            },
+            {
+                id: 3,
+                title: "c"
+            }
+        ]
+    })
 })
 
 /**
@@ -30,7 +57,7 @@ router.get('/json', async (ctx, next) => {
  */
 // router.get("/profile/:userName", async(ctx, next) => {
 //     const { userName } = ctx.params
-    
+
 //     ctx.body = {
 //         title: 'zzzhim',
 //         userName
@@ -39,7 +66,7 @@ router.get('/json', async (ctx, next) => {
 
 // router.get("/loadMore/:userName/:pageIndex", async(ctx, next) => {
 //     const { userName, pageIndex } = ctx.params
-    
+
 //     ctx.body = {
 //         title: 'zzzhim',
 //         userName,
