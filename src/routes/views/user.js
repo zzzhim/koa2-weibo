584fdf28d4f8d4579d7ec3faf7429da330d7d853
zzzhim <3218566389@qq.com>
2020-02-04 01:01:39 +0800

    feat: 添加登录状态

--- src/routes/views/user.js
+++ src/routes/views/user.js
@@ -1,7 +1,7 @@
 /*
  * Author: your name
  * Date: 2020-02-01 00:31:43
- * LastEditTime: 2020-02-03 20:51:15
+ * LastEditTime: 2020-02-04 01:01:07
  * LastEditors: Please set LastEditors
  * Description: In User Settings Edit
  * FilePath: \koa-weibo\src\routes\views\user.js
@@ -16,11 +16,36 @@ router.get('/', async (ctx, next) => {
 })
 
 router.get('/login', async (ctx, next) => {
-    await ctx.render('login', {})
+    // const userInfo = await ctx.session.userInfo
+
+    await ctx.render('login', {
+        // locals: {
+        //     ...(userInfo instanceof Object ? userInfo : {}),
+        //     isLogin: userInfo instanceof Object
+        // }
+        ...(await getLoginInfo(ctx))
+    })
 })
 
 router.get('/register', async (ctx, next) => {
-    await ctx.render('register', {})
+    await ctx.render('register', await getLoginInfo(ctx))
 })
 
 module.exports = router
+
+async function getLoginInfo(ctx) {
+    let data = {
+        isLogin: false
+    }
+
+    const userInfo = await ctx.session.userInfo
+
+    if(userInfo) {
+        data = {
+            isLogin: true,
+            ...userInfo
+        }
+    }
+    console.log(data)
+    return data
+}
